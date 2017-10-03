1f9e5b243f6570fbac0215a2d4f19bdb509224ba
Andres Morelos <andresmorelos123@gmail.com>
2017-10-03 09:09:24 -0500

    Actualizando script de compresión

--- compress.sh
+++ compress.sh
@@ -6,31 +6,30 @@ make_a_copy(){
 		rm -fr ../$NOMBRE
 		cp -r ../boletin ../$NOMBRE
 		rm ../$NOMBRE/compress.sh
-		cd ../$NOMBRE
+		cd ..
 		compress
 	else
 		cp -r ../boletin ../$NOMBRE
 		rm ../$NOMBRE/compress.sh
-		cd ../$NOMBRE
+		cd ..
 		compress
 	fi
 }
 
 delete_folder(){
-	cd ..
 	rm -fr $NOMBRE
 	exit
 }
 
 compress(){
 	if [ -s $NOMBRE.zip ]; then
-		rm ../$NOMBRE.zip
+		rm $NOMBRE.zip
 		echo "comprimiendo archivo $NOMBRE.zip" ;
-		zip -r ../$NOMBRE.zip  *
+		zip -r $NOMBRE.zip  $NOMBRE/*
 		delete_folder
 	else
 		echo "comprimiendo archivo $NOMBRE.zip" ;
-		zip -r ../$NOMBRE.zip *
+		zip -r $NOMBRE.zip $NOMBRE/*
 		delete_folder
 	fi
 }
